811a56d54e9d3fe563a4a10aa787d6e9f9b4d01a
Debbl <me@aiwan.run>
2022-09-28 11:47:47 +0800

    add user login

--- api/login.ts
+++ api/login.ts
@@ -2,19 +2,47 @@ import { Handler } from "@netlify/functions";
 import vikaLogin from "../vika/user/login";
 
 const handler: Handler = async (event, context) => {
-  if (event.httpMethod === "POST") {
-    const responese = await vikaLogin();
-    console.log(event.body);
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ body: event.body, responese }),
-    };
-  } else {
+  if (event.httpMethod !== "POST")
     return {
       statusCode: 200,
       body: JSON.stringify({ msg: "请使用 POST 方法请求" }),
     };
+
+  const responese = await vikaLogin();
+  const { username, password } = JSON.parse(event.body) as {
+    username: string;
+    password: string;
+  };
+  const records = responese.data.records;
+  for (let i = 0; i < records.length; i++) {
+    const record = records[i];
+    if (
+      username === record.fields.username &&
+      password === record.fields.password
+    ) {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          code: 200,
+          msg: null,
+          data: {
+            username: username,
+            password: "",
+          },
+          map: {},
+        }),
+      };
+    }
   }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      code: 200,
+      msg: "用户名或密码错误",
+      data: {},
+      map: {},
+    }),
+  };
 };
 
 export { handler };
--- vika/user/login.ts
+++ vika/user/login.ts
@@ -6,7 +6,6 @@ const vikaLogin = async () => {
   const datasheet = vika.datasheet("dstf2Lur3r2D7Zpz0v");
   // 分页获取记录，默认返回第一页
   const responese = await datasheet.records.query({ viewId: "viw7TBlhXHt9t" });
-  console.log(responese);
   return responese;
 };
 
