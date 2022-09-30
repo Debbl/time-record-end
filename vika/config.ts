b2a7bbbf15484693e52a4a9d6a4f83a498c463d1
Debbl <me@aiwan.run>
2022-09-30 16:04:46 +0800

    add cors

--- vika/config.ts
+++ vika/config.ts
@@ -7,6 +7,8 @@ const getResult = (): Response => ({
   headers: {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "*",
+    "Access-Control-Allow-Methods": "*",
   },
 });
 
