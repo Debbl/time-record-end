ff1804c3b48ad3ca0f0a9540c8d40cced3f9ebc5
Debbl <me@aiwan.run>
2022-10-01 22:14:38 +0800

    fix

--- api/time-get-user-week.ts
+++ api/time-get-user-week.ts
@@ -1,5 +1,5 @@
 import type { Handler } from "@netlify/functions";
-import type { IFieldValueMap } from "@vikadata/vika";
+import type { IFieldValue, IFieldValueMap } from "@vikadata/vika";
 import { getFormatTime, getResult } from "../vika/config";
 import { getEveryWeekTimePart } from "../vika/time/config";
 import vikaGetWeek from "../vika/time/getWeek";
@@ -34,11 +34,11 @@ const handler: Handler = async (event) => {
     const map = new Map<number, IFieldValueMap[]>();
     for (let i = 0; i < response.data.records.length; i++) {
       const timeStamp = response.data.records[i].fields.startTime;
-      const weekTime = timeArr.find(time => time[0] < timeStamp && time[1] > timeStamp);
+      const weekTime = timeArr.find(time => time[1] < timeStamp && time[2] > timeStamp);
       if (!map.has(weekTime[0])) map.set(weekTime[0], []);
       map.get(weekTime[0]).push(response.data.records[i].fields);
     }
-    const data = {};
+    const data: Record<number, { username: IFieldValue; time: string; totalTimeStamp: number }> = {};
     for (const [key, value] of map) {
       let totalTimeStamp = 0;
       for (const field of value)
