2e6994c647413167589cfa22185650332e24b8f6
Debbl <me@aiwan.run>
2022-10-04 22:07:30 +0800

    style: change prettier
    body
    
    footer

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
   tabWidth: 2, // 缩进的空格数
   useTabs: false, // 缩进使用空格，不适用制表符
   semi: true, // 语句末尾添加分号
-  singleQuote: false, // 使用单引号
+  singleQuote: false, // 使用双引号
   trailingComma: "all", // 尾随逗号
   bracketSpacing: true, // { foo: bar } 有空格
 };
